f5277881577ceaee2295b9fba397568f364bcb77
Minato Rokugo <mirokugo@c2r4s4.42tokyo.jp>
2024-04-24 09:36:11 +0900

    git hub

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,62 +6,35 @@
 /*   By: mirokugo <mirokugo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 05:55:00 by mirokugo          #+#    #+#             */
-/*   Updated: 2024/04/20 12:44:41 by mirokugo         ###   ########.fr       */
+/*   Updated: 2024/04/24 09:19:08 by mirokugo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <limits.h>
 
-int	digicheck(long long nb, int sign)
-{
-	nb = nb * sign;
-	if (nb == (long long)INT_MAX + 1)
-		return (INT_MIN);
-	if (nb == (long long)INT_MIN - 1)
-		return (INT_MAX);
-	if (nb > INT_MAX && nb < UINT_MAX)
-		return (nb - 4294967296);
-	if (nb > INT_MAX && nb < 9223372036854775807)
-		return (nb - 4294967296);
-	if (nb > LONG_MAX)
-		return (-1);
-	if (nb < INT_MIN && nb > -9223372036854775807)
-		return (nb + 4294967296);
-	if (nb < LONG_MIN)
-		return (nb);
-	return (nb);
-}
-
 int	ft_atoi(const char *str)
 {
-	long long	nb;
-	int			sign;
+	int		sign;
+	long	num;
 
-	nb = 0;
 	sign = -1;
+	num = 0;
 	while (*str == ' ' || (*str >= 9 && *str <= 13))
 		str++;
-	if (*str == '+' || *str == '-')
-	{
-		if (*str == '-')
-			sign = 1;
-		else
-			sign = -1;
+	if (*str == '-')
+		sign = 1;
+	if (*str == '-' || *str == '+')
 		str++;
-	}
 	while (*str >= '0' && *str <= '9')
 	{
-		if (nb < LONG_MIN / 10 || (nb == LONG_MIN / 10 && (*str - '0') > 8))
-			return (-1);
-		if (nb > LONG_MAX / 10 || (nb == LONG_MAX / 10 && (*str - '0') > 7))
-			return (-1);
-		nb = nb * 10 - (*str - '0');
+		if (num * sign < INT_MIN)
+			return (INT_MIN);
+		if (num * sign > INT_MAX)
+			return (INT_MAX);
+		num = num * 10 + (*str - '0');
 		str++;
 	}
-	if (nb == LONG_MIN && sign == -1)
-		return (-1);
-	nb = digicheck(nb, sign);
-	return (nb);
+	return (sign * num);
 }
 
 /* #include <libc.h>
